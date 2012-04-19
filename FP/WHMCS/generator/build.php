6bfcafe720bf8eac87d23f28d5f692075192fd5c
SnatchFrigate <snatchfrigate@gmail.com>
2012-04-19 21:29:05 +0100

    fixed build.php writing to the incorrect path
    build.php now creates paths recursively

--- FP/WHMCS/generator/build.php
+++ FP/WHMCS/generator/build.php
@@ -52,11 +52,11 @@ if (!isset($argv[2]))
   echoln("please specify a namespace");
 }
 
-$namespace = $argv[2];
+$namespace = @$argv[2];
 $force = in_array('-f', $argv);
-$output_dir = isset($argv[3]) ? $argv[3] : __DIR__;
+$output_dir = isset($argv[3]) ? $argv[3] : '.';
 
-foreach(glob(sprintf('%s/configurations/%s/*.json', __DIR__, $argv[1])) as $spec_path)
+foreach(glob(sprintf('%s/configurations/%s/*.json', __DIR__, @$argv[1])) as $spec_path)
 {
   $spec_file_name = basename($spec_path);
   $spec = json_decode(file_get_contents($spec_path), true);
@@ -67,8 +67,8 @@ foreach(glob(sprintf('%s/configurations/%s/*.json', __DIR__, $argv[1])) as $spec
     continue;
   }
   
-  @mkdir($output_dir.'/Entity');
-  @mkdir($output_dir.'/Type');
+  @mkdir($output_dir.'/Entity', true);
+  @mkdir($output_dir.'/Type', true);
   file_put_contents(sprintf('%s/Entity/%s.php', $output_dir, $spec['className']), parseTemplate('Entity', $spec));
   file_put_contents(sprintf('%s/Type/%s.php', $output_dir, $spec['className']), parseTemplate('Type', $spec));
 }
